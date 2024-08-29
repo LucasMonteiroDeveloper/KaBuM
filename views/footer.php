f1333fd39fe967d5e2dd29617bc67921cde83419
Lucas Monteiro <monteiro.dlucas@gmail.com>
2024-08-29 01:31:43 -0300

    Created system and configured database

---
+++ views/footer.php
@@ -0,0 +1,30 @@
+<footer>
+	<div class="containerFooter flexColumn">
+		<div class="footer_menu">
+			<div class="fm_1">
+				<a href="">
+					<p></p>
+				</a>
+			</div>
+			<div class="fm_2">
+				<a href="">
+					<p></p>
+				</a>
+			</div>
+			<div class="fm_3">
+				<a href="">
+					<p></p>
+				</a>
+			</div>
+		</div>
+
+		<div class="footer_area">
+			
+		</div>
+
+		<div class="footer_copy">
+			Coyright - Todos os direitos reservados
+		</div>
+
+	</div>
+</footer>
---
+++ views/home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <div class="container mt-5">
+        <h1>Bem-vindo à nossa página inicial!</h1>
+        <p>Conteúdo da página inicial.</p>
+    </div>
+</body>
+</html>
